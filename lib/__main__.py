8f344001a909a736a9c452acc4ed314383f070d2
Georgiy Karazhev <karazhev.g@gmail.com>
2024-03-26 00:11:57 +0500

    Add config file's dn req.

---
+++ lib/__main__.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import argparse
+import importlib
+import os
+import sys
+
+
+def main():
+    print("TelemetryXplorer")
+
+
+if __name__ == '__main__':
+    main()
